ab26149436ed4ff77ef5e86622f5ef4505984981
Matheus Silva <103164463+MSOliver1998@users.noreply.github.com>
2023-06-12 12:44:39 -0300

    fix/style

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,12 +3,19 @@ import { MdOutlineKeyboardArrowUp } from 'react-icons/md'
 
 const Footer=()=>{
 
+    const goToTop=()=>{
+        window.scroll({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+
     return(
         <footer>
             <h1>Motors shop</h1>
             <p>&copy; 2022 -  Todos os direitos reservados.</p>
             <div>
-                <i><MdOutlineKeyboardArrowUp/></i>
+                <i onClick={goToTop} className="top"><MdOutlineKeyboardArrowUp/></i>
             </div>
         </footer>
     )
